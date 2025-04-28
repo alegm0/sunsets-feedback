be3d7c83f8d8795d7f020ae20ffec746b2a19d96
Alejandra <alejandragm272415@gmail.com>
2025-04-28 10:24:47 +1000

    Setup env variables for production

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,22 +3,20 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-
-
-// Tu configuración de Firebase
+// Configuración de Firebase usando variables de entorno
 const firebaseConfig = {
-  apiKey: "AIzaSyDG798w6agqB_bKJDMYlG8-ixsACgx5DYw",
-  authDomain: "sunsets-feedback.firebaseapp.com",
-  projectId: "sunsets-feedback",
-  storageBucket: "sunsets-feedback.firebasestorage.app",
-  messagingSenderId: "119088001424",
-  appId: "1:119088001424:web:2284f89122f11969910315",
-  measurementId: "G-98KZKP7VHQ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Inicializar Firebase y Firestore
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-export { db, auth };
 
+export { db, auth };
